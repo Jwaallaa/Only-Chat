f4024941ed6f2d8ccc997fa1e6d1dff97f5d0392
Udit jaiswal <119353262+Jwaallaa@users.noreply.github.com>
2024-11-13 20:41:40 +0530

    "Updated App.jsx: replaced createHashRouter with createBrowserRouter, changed route paths, and reformatted code."

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,31 @@
-import { createHashRouter, RouterProvider } from 'react-router-dom'; // Use createHashRouter for hash-based routing
-import './App.css';
-import Home from './Components/Home';
-import Chats from './Components/Chats';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import './App.css'
+import Home from './Components/Home'
+import Chats from './Components/Chats'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/chats",
-    element: <Chats />
+    path: "/Only-Chat",
+    element : <Home/>
   }
-], {
+  ,
+  {
+    path : "/Only-Chat/chats",
+    element: <Chats/>
+  }
+
+] ,{
   future: { v7_startTransition: true }, // Enable the future flag
-});
+})
 
 function App() {
+  
+
   return (
     <>
-      <RouterProvider router={router} />
+      <RouterProvider router = {router}/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
